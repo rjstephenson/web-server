cc0c0c9e1978416b444dea8f84ac45aa4c2bbf21
Randy Stephenson <randystephenson@gmail.com>
2016-06-01 11:03:47 -0400

    Update Language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req,res) {
-	res.send("Hello Express!");
+	res.send("About Us!");
 });
 
 app.use(express.static(__dirname + '/public'));
