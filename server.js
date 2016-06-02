d0339a357dfeaac3fadd15addeca2bd20532146d
Stephenson <rstephenson@chubb.com>
2016-06-01 23:17:54 -0400

    Update port for horoku

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js'); 
-
-// var middleware = {
-// 	requireAuthentication: function(req, res, next) {
-// 		console.log('private route hit!');
-// 		next();
-
-// 	},
-// 	logger: function(req, res, next) {
-// 		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-// 		next();
-// 	}
-// }
 
 app.use(middleware.logger);
 
